ef1969ddc300596c6eb266c9dc2594f2a488728e
pradeepshridevi <pp3518072@gmail.com>
2025-03-10 16:05:01 +0530

    Create rate of intrest.py

---
+++ rate of intrest.py
@@ -0,0 +1,5 @@
+principalamount=int(input("enter amount"))
+years = int(input("enter years"))
+rateofintrest=float(input("enter rate of intrest per annum"))
+simpleintrest=principalamount*years*rateofintrest/100
+print(simpleintrest)
