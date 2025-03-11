ff85132534156c2e54d8921e0fb27b0581b215e6
pradeepshridevi <pp3518072@gmail.com>
2025-03-11 09:40:48 +0530

    Create avg of three nunbers.py

---
+++ avg of three nunbers.py
@@ -0,0 +1,6 @@
+num1 = float(input("enter first number: "))
+num2 float (input ("enter second number: "))
+num3 = float (input ("enter third number : "))
+print("pradeep")
+average (num1 + num2 + num3)
+print("The average is:", average)
