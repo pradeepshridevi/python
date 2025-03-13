c7450586d5d574eb9ca98766645cab3151ff6129
pradeepshridevi <pp3518072@gmail.com>
2025-03-13 15:17:55 +0530

    Create  Accessing Dictionary Values.py

---
+++ Accessing Dictionary Values.py
@@ -0,0 +1,11 @@
+my_dict = {
+    "name": "Pradeep",
+    "age": 18,
+    "city": "Tumkur"
+}
+name = my_dict["name"]
+age = my_dict["age"]
+city = my_dict["city"]
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
